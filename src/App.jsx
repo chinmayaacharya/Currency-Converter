44e2f109394d4c4c4fbbe5c90a3ed09279d41a22
M Chinmaya Acharya <acharyachinmaya695@gmail.com>
2024-01-26 13:47:28 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
-import {InputBox} from './components/index.js'
+import {InputBox} from './Components/index.js'
 
 function App() {
   const [amount, setAmount] = useState(0)
